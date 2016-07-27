edecad8856a45aec1ca9e75b6fdb8368a6de411e
Nurzhan <nurzhan@tuta.io>
2016-07-27 09:11:06 +0600

    set apart proxy handlers

--- index.js
+++ index.js
@@ -5,37 +5,41 @@ module.exports = function(apiKey) {
   mobizonObj = {
     apiKey: apiKey
   }
-  mobizonProxy = new Proxy(mobizonObj, {
-    get(target, apiModule) {
-      mobizonModuleObj = {
-        apiKey: target.apiKey,
-        apiModule: apiModule
+  mobizonProxy = new Proxy(mobizonObj, mobizonProxyHandler);
+  return mobizonProxy;
+}
+
+mobizonProxyHandler = {
+  get: function(target, apiModule) {
+    mobizonModuleObj = {
+      apiKey: target.apiKey,
+      apiModule: apiModule
+    };
+    mobizonModuleProxy = new Proxy(mobizonModuleObj, mobizonModuleProxyHandler);
+    return mobizonModuleProxy;
+  }
+}
+
+mobizonModuleProxyHandler = {
+  get: function(target, apiMethod) {
+    mobizonRequest = function(queryString, callbackFunction){
+      if (!queryString) {
+        queryString = {};
+      }
+      queryString.apiKey = target.apiKey;
+      var request_options = {
+        baseUrl: 'https://api.mobizon.com/service/',
+        url: target.apiModule + '/' + apiMethod,
+        followRedirect: false,
+        json: true,
+        qs: queryString
       };
-      mobizonModuleProxy = new Proxy(mobizonModuleObj, {
-        get(target, apiMethod) {
-          mobizonRequest = function(queryString, callbackFunction){
-            if (!queryString) {
-              queryString = {}
-            }
-            queryString.apiKey = target.apiKey;
-            var request_options = {
-              baseUrl: 'https://api.mobizon.com/service/',
-              url: target.apiModule + '/' + apiMethod,
-              followRedirect: false,
-              json: true,
-              qs: queryString
-            };
-            request(request_options, function (error, response, body) {
-              if (callbackFunction) {
-                callbackFunction(error, body);
-              }
-            })
-          }
-          return mobizonRequest;
+      request(request_options, function (error, response, body) {
+        if (callbackFunction) {
+          callbackFunction(error, body);
         }
       })
-      return mobizonModuleProxy;
     }
-  })
-  return mobizonProxy;
+    return mobizonRequest;
+  }
 }
