7e2506351a252199dc0907743be265c4621f05c9
Nurzhan <nurzhan@tuta.io>
2016-08-03 19:20:11 +0600

    little refactoring

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ var request = require('request');
 var Reflect = require('harmony-reflect');
 
 module.exports = function(apiKey) {
-  mobizonObj = {
+  let mobizonObj = {
     apiKey: apiKey
   }
-  mobizonProxy = new Proxy(mobizonObj, mobizonProxyHandler);
+  let mobizonProxy = new Proxy(mobizonObj, mobizonProxyHandler);
   return mobizonProxy;
 }
 
-mobizonProxyHandler = {
+let mobizonProxyHandler = {
   get: function(target, apiModule) {
-    mobizonModuleObj = {
+    let mobizonModuleObj = {
       apiKey: target.apiKey,
       apiModule: apiModule
     };
-    mobizonModuleProxy = new Proxy(mobizonModuleObj, mobizonModuleProxyHandler);
+    let mobizonModuleProxy = new Proxy(mobizonModuleObj, mobizonModuleProxyHandler);
     return mobizonModuleProxy;
   }
 }
 
-mobizonModuleProxyHandler = {
+let mobizonModuleProxyHandler = {
   get: function(target, apiMethod) {
-    mobizonMethod = function(queryString, callbackFunction){
+    let mobizonMethod = function(queryString, callbackFunction){
       if (!queryString) {
         queryString = {};
       }
-      isJsonQuery = !(queryString.output && queryString.output != 'json') // mobizon support XML too, but by default mobizon use JSON
+      let isJsonQuery = !(queryString.output && queryString.output != 'json') // mobizon support XML too, but by default mobizon use JSON
+
       queryString.apiKey = target.apiKey;
-      var request_options = {
+
+      let request_options = {
         baseUrl: 'https://api.mobizon.com/service/',
         url: target.apiModule + '/' + apiMethod,
         followRedirect: false,
@@ -37,15 +39,17 @@ mobizonModuleProxyHandler = {
       };
       request(request_options, function (requestError, requestResponse, requestBody) {
         if (callbackFunction) {
+          let error = null;
+          let data = null;
+
           if (requestError || requestResponse.statusCode != 200) {
             error = {
               code: 999,
-              message: 'No network connection or cannot reach the server'
+              message: 'Не удалось установить связь с сервером'
             }
-            callbackFunction(error, null);
           }
           else if (!isJsonQuery) {
-            callbackFunction(null, requestBody)
+            data = requestBody;
           }
           else if (requestBody.code != 0) {
             error = {
@@ -53,18 +57,18 @@ mobizonModuleProxyHandler = {
               message: requestBody.message,
               data: requestBody.data
             }
-            callbackFunction(error, null);
           }
           else if (requestBody.code == 0) {
-            callbackFunction(null, requestBody.data)
+            data = requestBody.data;
           }
           else {
             error = {
               code: 500,
               message: 'Неизвестная ошибка приложения'
             }
-            callbackFunction(error, null)
           }
+
+          callbackFunction(error, data);
         }
       })
     }
